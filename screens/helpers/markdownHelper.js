246f92169bc2a09d540da4014459dde6e13529e6
1hitsong <3330318+1hitsong@users.noreply.github.com>
2023-07-01 20:57:06 -0400

    Fix markdown bug, scroll to top using 3 fingers

--- screens/helpers/markdownHelper.js
+++ screens/helpers/markdownHelper.js
@@ -1,3 +1,5 @@
+import { Text } from 'react-native'
+
 export const markdownRules = {
     list_item: (node, children, parent, styles) =>
         <Text key={node.key} style={[styles.list_item]}> • {children}</Text>
